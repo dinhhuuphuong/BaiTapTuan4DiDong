adee7daff063bcff6feb9dcb8cd5fa497fef3857
dinhhuuphuong <muoi1772002@@gmail.com>
2023-10-01 16:58:26 +0700

    bai tap lap trinh di dong tuan 4

---
+++ App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { View } from 'react-native';
+import {styles} from './style';
+import { Layout } from './src/component/1e/Layout';
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Layout />
+    </View>
+  );
+}
---
+++ src/component/1b/style.ts
@@ -0,0 +1,19 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor:'#C7F4F6'
+  },
+  header: {
+    flex: 0.5,
+    justifyContent: "center",
+    alignItems:'center',
+  },
+  body:{
+    flex:0.4,
+  },
+  footer:{
+    flex:0.5
+  }
+});
